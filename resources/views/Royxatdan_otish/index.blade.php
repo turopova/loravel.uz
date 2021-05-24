95f4c550209fe92244e8c6c1aab1b64325a9e663
TDildora <turopovadildora@gmail.com>
2021-05-24 16:53:40 +0500

    Kommitlar

--- resources/views/Royxatdan_otish/index.blade.php
+++ resources/views/Royxatdan_otish/index.blade.php
@@ -2,14 +2,17 @@
 
 @section('content')
 
-<form action="{{ route('Royxatdan_otish') }}" method="post">
-    @csrf
-    Ism: <input type="text" name="ism">
-    Familya: <input type="text" name="familya">
-    Sharif: <input type="text" name="o_ism">
-    To'g'ilgan sana: <input type="date" name="tug_sana" id="tug_sana">
-    Passport seriya: <input type="text" name="passport">
-    Karta raqam: <input type="text" name="karta_raqam">
-    kirish: <input type="button" value="ok" action="#intro">
-</form>
+    <div>
+        <form action="{{ route('Royxatdan_otish') }}" method="post">
+            @csrf
+            Ism: <input type="text" name="ism" >
+            Familya: <input type="text" name="familya" >
+            Sharif: <input type="text" name="o_ism" >
+            To'g'ilgan sana:<br> <input type="date" name="tug_sana" ><br>
+            Passport seriya: <input type="text" name="passport">
+            Karta raqam: <input type="text" name="karta_raqam">
+            kirish: <input type="button" value="ok" action="#intro">
+        </form>
+    </div>
+
 @endsection
